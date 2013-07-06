843396cef8b30d232a5b637ced0f7e03af8d406e
Telmo Pires <taapires@tp-mbp.lan>
2013-07-06 17:28:30 +0100

    update to latest version

--- js/tp.js
+++ js/tp.js
@@ -1,6 +1,15 @@
 $(document).ready(function () {
 "use strict";
 
+/*$('nav ul li a').hover(function(){
+    if ($(this).hasClass('menu-select')) {
+        $(this).css('color', 'white');
+    } else {
+        $(this).css('color', 'color: rgba(86,247,189,1)');
+    }
+});*/
+
+
 function clickWork() {
             $menuWork.addClass('menu-select');
             $menuContact.removeClass('menu-select');
@@ -20,11 +29,12 @@ function clickWork() {
 
     var bn = $("#bottom-nav");
     var main = $('#main-content').html();
-    var profile = '<div id="lateral" class="pull-left"><p>PROFILE</p></div><div id="lateral-dir" class="pull-right"><p class="bold">Name</p><p class="nbold">Daniela Pereira</p><p class="bold">Born</p><p class="nbold">July 87</p><p class="bold">Nationality</p><p class="nbold">Portuguese</p><p class="bold">Interests</p><p class="nbold">Photography . Design . Illustration . Music . Cinema</p></div><div class="bottom-nav"><br><div class="square-active square-profile"></div><div class="square-inactive square-edu"></div><div class="square-inactive square-skills"></div></div>';
-    var skills = '<div id="lateral" class="pull-left"><p>SKILLS</p></div><div id="lateral-dir" class="pull-right"><p class="bold">Graphic Design</p><p class="nbold">Adobe Illustrator . Adobe Indesign</p><p class="bold">Photography</p><p class="nbold">Adobe Photoshop</p><p class="bold">Video</p><p class="nbold">Avid . Adobe After Effects</p><p class="bold">3D</p><p class="nbold">Blender</p><p class="bold">Web</p><p class="nbold">Adobe Dreamweaver . Adobe Flash</p></div><div class="bottom-nav"><br><div class="square-inactive square-profile"></div><div class="square-inactive square-edu"></div><div class="square-active square-skills"></div></div>';
-    var edu = '<div id="lateral" class="pull-left"><p>EDUCATION</p></div><div id="lateral-dir" class="pull-right"><p class="bold">school of communication, arts and information technology | lusófona</p><p class="nbold">post graduate | design</p><p class="bold">higher school of communication and media studies | ipl</p><p class="nbold">graduate | audiovisual and multimedia</p><p class="bold">lxschool</p><p class="nbold">master graphic designer</p><p class="bold">iefp</p><p class="nbold">macromedia flash and actionscript 2</p></div><div class="bottom-nav"><br><div class="square-inactive square-profile"></div><div class="square-active square-edu"></div><div class="square-inactive square-skills"></div></div>';
+    var profile = '<div id="lateral" class="pull-left"><p>PROFILE</p></div><div id="lateral-dir" class="pull-right"><p class="bold">Name</p><p class="nbold">Daniela Pereira</p><p class="bold">Born</p><p class="nbold">July &rsquo;87</p><p class="bold">Nationality</p><p class="nbold">Portuguese</p><p class="bold">Interests</p><p class="nbold">Photography . Design . Illustration . Music . Cinema</p></div><div class="bottom-nav"><br><div class="square-active square-profile"></div><div class="square-inactive square-work"></div><div class="square-inactive square-edu"></div><div class="square-inactive square-skills"></div></div>';
+    var workTab = '<div id="lateral" class="pull-left"><p>WORK</p></div><div id="lateral-dir" class="pull-right"><p class="bold">UPPARTNER | THE TOUCH POINT AGENCY</p><p class="nbold">APRIL&rsquo; 12 _ SEPTEMBER&rsquo; 12</p><p class="bold">SHARKBITE | INTEGRATED COMMUNICATION</p><p class="nbold">APRIL&rsquo; 10 _ SEPTEMBER&rsquo; 11</p></div><div class="bottom-nav"><br><div class="square-inactive square-profile"></div><div class="square-active square-work"></div><div class="square-inactive square-edu"></div><div class="square-inactive square-skills"></div></div>';    
+    var edu = '<div id="lateral" class="pull-left"><p>EDUCATION</p></div><div id="lateral-dir" class="pull-right"><p class="bold">school of communication, arts and information technology | lusófona</p><p class="nbold">post graduate | design</p><p class="bold">higher school of communication and media studies | ipl</p><p class="nbold">graduate | audiovisual and multimedia</p><p class="bold">lxschool</p><p class="nbold">master graphic designer</p><p class="bold">iefp</p><p class="nbold">macromedia flash and actionscript 2</p></div><div class="bottom-nav"><br><div class="square-inactive square-profile"></div><div class="square-inactive square-work"></div><div class="square-active square-edu"></div><div class="square-inactive square-skills"></div></div>';
+    var skills = '<div id="lateral" class="pull-left"><p>SKILLS</p></div><div id="lateral-dir" class="pull-right"><p class="bold">Graphic Design</p><p class="nbold">Adobe Illustrator . Adobe Indesign</p><p class="bold">Photography</p><p class="nbold">Adobe Photoshop</p><p class="bold">Video</p><p class="nbold">Avid . Adobe After Effects</p><p class="bold">3D</p><p class="nbold">Blender</p><p class="bold">Web</p><p class="nbold">Adobe Dreamweaver . Adobe Flash</p></div><div class="bottom-nav"><br><div class="square-inactive square-profile"></div><div class="square-inactive square-work"></div><div class="square-inactive square-edu"></div><div class="square-active square-skills"></div></div>';
     var contactLinks = '<div id="lateral" class="pull-left"><p>FOLLOW</p></div><a href="http://www.flickr.com/photos/dpdanielapereira/" target="_blank"><div class="lateral-contacts pull-right contact-flickr"></div></a><a href="http://pt.linkedin.com/pub/daniela-pereira/27/5b5/4b/pt" target="_blank"><div class="lateral-contacts pull-right contact-linkedin"></div></a><a href="http://vimeo.com/user2372629" target="_blank"><div class="lateral-contacts pull-right contact-vimeo"></div></a><a href="http://instagram.com/instagramgramstyle" target="_blank"><div class="lateral-contacts pull-right contact-insta"></div></a>';
-
+   
     $(".work1").click(function () {
         $.fancybox.open([{
             href: "img/works/agendaola_1.jpg"
@@ -451,6 +461,12 @@ function clickWork() {
         });
 
         // WORK
+        $mainContent.on('click', '.square-work', function(){
+            $mainContent.hide().fadeIn(1000);
+            $mainContent.html(workTab);
+        });
+
+        // WORKS
         $menuWork.on('click', clickWork);
 
     });
